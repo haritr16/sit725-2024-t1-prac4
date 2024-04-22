ed6598e567ab0483c4d2db9d227be21ff5838b15
haritr16 <s223295149@deakin.edu.au>
2024-04-23 00:21:03 +1000

    adding db

--- routes/formRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const formController = require('../controllers/formController');
-
-router.post('/submit', formController.form_submit);
-router.get('/forms', formController.get_forms);
-
-module.exports = router;
-
--- server.js
+++ server.js
@@ -1,30 +1,54 @@
-
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const formRoutes = require('./routes/formRoutes');
 
 const app = express();
 
-// MongoDB Connection
+app.use(express.static(__dirname+'/public'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// connection to MongoDB
 const mongoURI = 'mongodb+srv://s223295149:cMLGEplq4XoycbeA@cluster0.uus7rft.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 mongoose.connect(mongoURI)
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.log(err));
 
-// Middleware
-app.use(express.static(__dirname + '/public'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+//  a mongoose schema
+const Schema = mongoose.Schema;
+const FormSchema = new Schema({
+    name: String,
+    email: String,
+    car: String
+});
 
-// Routes
-app.use('/', formRoutes);
+// Define a scheam called form to store form data
+const Form = mongoose.model('Form', FormSchema);
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+// API to handle form submission
+app.post('/submit', (req, res) => {
+    const { name, email, car } = req.body;
+    const newForm = new Form({ name, email, car });
+    newForm.save()
+        .then(() => res.send('Form submitted successfully'))
+        .catch(err => console.log(err));
+});
 
 // Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+
+
 
 
 
